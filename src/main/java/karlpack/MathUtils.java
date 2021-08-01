24acae84691265e218a9e853888ea63352968e01
Karlroxas21 <karlmarxroxas1@gmail.com>
2021-08-01 08:31:46 +0800

    second commit

--- src/main/java/karlpack/MathUtils.java
+++ src/main/java/karlpack/MathUtils.java
@@ -4,4 +4,5 @@ public class MathUtils {
 
     public int add(int a, int b){return a + b;}
     public double areaOfCircle(double area){return  Math.PI * area * area;}
+    public int divide(int a, int b){return a / b;}
 }
