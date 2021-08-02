6c7554e52151711dfcda708ddc51e54b280fec23
Karlroxas21 <karlmarxroxas1@gmail.com>
2021-08-02 21:53:20 +0800

    commit

--- src/test/java/karlpack/MathUtilsTest.java
+++ src/test/java/karlpack/MathUtilsTest.java
@@ -2,21 +2,35 @@ package karlpack;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MathUtilsTest {
+    MathUtils mathUtils;
+
+    @BeforeEach
+    void  mathUtils(){
+        mathUtils = new MathUtils();
+    }
+
+    
     @Test
     void addTest() {
-        MathUtils mathUtils = new MathUtils();
         int expected = 10;
         int actual = mathUtils.add(5,5);
         assertEquals(expected, actual, "add Method should sum 10");
     }
     @Test
     void areaOfCircleTest(){
-        MathUtils mathUtils = new MathUtils();
         double  expected =  314.1592653589793;
         double actual = mathUtils.areaOfCircle(10);
         assertEquals(expected, actual);
+    }
+    @Test
+    void divideTest(){
+        assertThrows(ArithmeticException.class, () ->mathUtils.divide(1, 0),"If 0, Should throw");
+
+
     }
 }
