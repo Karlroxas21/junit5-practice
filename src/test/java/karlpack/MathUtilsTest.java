03e46e49b652bd0f6bc8505836323b8593837d98
Karlroxas21 <karlmarxroxas1@gmail.com>
2021-08-04 15:31:30 +0800

    22/27

--- src/test/java/karlpack/MathUtilsTest.java
+++ src/test/java/karlpack/MathUtilsTest.java
@@ -1,36 +1,80 @@
 package karlpack;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MathUtilsTest {
     MathUtils mathUtils;
 
     @BeforeEach
-    void  mathUtils(){
+    void mathUtils() {
         mathUtils = new MathUtils();
     }
 
-    
+
     @Test
+    @DisplayName("Karl Test") //display name in test
+    @Disabled
+        //disable the test
     void addTest() {
         int expected = 10;
-        int actual = mathUtils.add(5,5);
+        int actual = mathUtils.add(5, 5);
         assertEquals(expected, actual, "add Method should sum 10");
     }
+
     @Test
-    void areaOfCircleTest(){
-        double  expected =  314.1592653589793;
+    void areaOfCircleTest() {
+        double expected = 314.1592653589793;
         double actual = mathUtils.areaOfCircle(10);
         assertEquals(expected, actual);
     }
-    @Test
-    void divideTest(){
-        assertThrows(ArithmeticException.class, () ->mathUtils.divide(1, 0),"If 0, Should throw");
 
+    @Test
+//    @EnabledOnJre(JRE.JAVA_8) // enable on specific JRE only.
+    @EnabledOnOs(OS.LINUX)
+        // enable on specific OS.
+    void divideTest() {
+//        assumeTrue(true); // if true, the test runs, vice versa if true.
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "If 0, Should throw");
+    }
+    @Nested
+    @DisplayName("Nested")
+    class nestMeTest {
+
+        @Test
+//        @DisplayName("Karl Test") //display name in test
+//        @Disabled //disable the test
+        void addTest() {
+            int expected = 10;
+            int actual = mathUtils.add(5,5);
+            assertEquals(expected, actual, "add Method should sum 10");
+        }
+        @Test
+        void areaOfCircleTest(){
+            double  expected =  314.1592653589793;
+            double actual = mathUtils.areaOfCircle(10);
+            assertEquals(expected, actual);
+        }
+    }
+    @Test
+    @DisplayName("assertAll")
+    void testMultiply(){
+        assertAll(
+                // [() ->]is lambda
+                () -> assertEquals(4, mathUtils.multiply(2,2)),
+                () -> assertEquals(10, mathUtils.multiply(5,2)),
+                () -> assertEquals(25, mathUtils.multiply(5,5)),
+                () -> assertEquals(100, mathUtils.multiply(10,10))
+        );
 
     }
 }
+
+
+
+
+
